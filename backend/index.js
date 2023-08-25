a7fb03e51d7e360e92994644f6eb22c7280e6789
Deni Shaipov <deni.shaipp@gmail.com>
2023-08-25 20:48:46 +0300

    added genres fetch on both backend/frontend

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import jsdom from 'jsdom';
 
 const app = express();
 const port = 3002;
@@ -12,6 +13,14 @@ app.get('/games', async (req, res) => {
   let json = await response.json();
   res.send(json);
 });
+
+app.get('/genres', async (req, res) => {
+  let response = await fetch('https://www.freetogame.com/api-doc');
+  let text = await response.text();
+  const html = new jsdom.JSDOM(text);
+  let genres = html.window.document.querySelector(".modal-body").textContent;
+  res.send(genres.trim().split(', '));
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
