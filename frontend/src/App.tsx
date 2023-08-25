77a8a71f4847f9504e501591b95e1e0e02b30635
Deni Shaipov <deni.shaipp@gmail.com>
2023-08-25 19:04:02 +0300

    pagination is sticky now

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Card, List, Spin, Typography, Empty, Button, Popover, Space } from 'antd';
+import { Card, List, Spin, Typography, Empty, Button, Popover, Space, Layout, Pagination } from 'antd';
 import './App.css'
 
+const { Header, Content, Footer } = Layout;
 const { Title } = Typography;
 const { Meta } = Card;
 
@@ -61,6 +62,8 @@ function App() {
   
   let [gamesList, setGamesList] = useState<IGame[]>([]);
   let [hasError, setHasError] = useState(false);
+  let [page, setPage] = useState(1);
+  let [pageSize, setPageSize] = useState(10);
 
   if (gamesList.length === 0 && hasError) {
     return (
@@ -82,43 +85,55 @@ function App() {
   } else {
     return (
       <div className="App">
-      <Title style={{textAlign: 'center', color: 'rgb(119, 119, 188)'}}>Games</Title>
-        <List
-          grid={{
-            gutter: 16,
-            xs: 1,
-            sm: 2,
-            md: 2,
-            lg: 3,
-            xl: 3,
-            xxl: 4,
-          }}
-          pagination={{ position: 'bottom', align: 'center' }}
-          dataSource={gamesList}
-          renderItem={(item) => (
-            <List.Item>
-              <Card title={item.title} hoverable cover={
-                <img alt={item.title} src={item.thumbnail} />
-              }>
-                <Meta description={
-                  <div style={{ fontSize: "1.1em" }}>
-                    <Space size="small" wrap>
-                      <Popover content="genre">
-                        <Button type="primary" style={{backgroundColor: "#f50"}} size="small">{item.genre}</Button>
-                      </Popover>
-                      <Popover content="publisher">
-                        <Button type="primary" style={{backgroundColor: "#f50"}} size="small">{item.publisher}</Button>
-                      </Popover>
-                      <Popover content="release date">
-                        <Button type="primary" style={{backgroundColor: "#f50"}} size="small">{formatDate(item.release_date)}</Button>
-                      </Popover>
-                    </Space>
-                  </div>
-                }/>
-              </Card>
-            </List.Item>
-          )}
-        ></List>
+        <Layout>
+          <Header className="mainHeader">
+            <Title className="mainTitle">Games</Title>
+          </Header>
+          <Content>
+            <List
+              grid={{
+                gutter: 16,
+                xs: 1,
+                sm: 2,
+                md: 2,
+                lg: 3,
+                xl: 3,
+                xxl: 4,
+              }}
+              // pagination={{ position: 'bottom', align: 'center' }}
+              dataSource={gamesList.slice((page - 1) * pageSize, page * pageSize)}
+              renderItem={(item) => (
+                <List.Item>
+                  <Card title={item.title} hoverable cover={
+                    <img alt={item.title} src={item.thumbnail} />
+                  }>
+                    <Meta description={
+                      <div style={{ fontSize: "1.1em" }}>
+                        <Space size="small" wrap>
+                          <Popover content="genre">
+                            <Button type="primary" style={{backgroundColor: "#f50"}} size="small">{item.genre}</Button>
+                          </Popover>
+                          <Popover content="publisher">
+                            <Button type="primary" style={{backgroundColor: "#f50"}} size="small">{item.publisher}</Button>
+                          </Popover>
+                          <Popover content="release date">
+                            <Button type="primary" style={{backgroundColor: "#f50"}} size="small">{formatDate(item.release_date)}</Button>
+                          </Popover>
+                        </Space>
+                      </div>
+                    }/>
+                  </Card>
+                </List.Item>
+              )}
+            ></List>
+          </Content>
+          <Footer className="mainFooter" style={{ position: 'sticky', zIndex: 1, bottom: 0, width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Pagination total={gamesList.length} onChange={(page, pageSize) => {
+              setPage(page);
+              setPageSize(pageSize);
+            }} />
+          </Footer>
+        </Layout>
     </div>
     )
   }
