9431f31cd99a11ff0b07f8ed86248b36cbc57af4
Deni Shaipov <deni.shaipp@gmail.com>
2023-08-25 10:29:04 +0300

    games are now read from server, now rendering list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { Card, List } from 'antd';
 
 function App() {
+  useEffect(() => {
+    (async () => {
+      let gamesListResponse = await fetch('http://localhost:3002/games');
+      let gamesListJson = await gamesListResponse.json();
+      setGamesList(gamesListJson);
+
+      console.log(gamesListResponse.status);
+      console.log(gamesListJson);
+    })();
+  }, []);
+
+  let [gamesList, setGamesList] = useState([]);
+
   return (
     <div className="App">
-      App
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 4,
+            lg: 4,
+            xl: 6,
+            xxl: 3,
+          }}
+          dataSource={gamesList}
+          renderItem={(item) => (
+            <List.Item>
+              <Card title={item.title}>Card content</Card>
+            </List.Item>
+          )}
+        />
     </div>
   );
 }
