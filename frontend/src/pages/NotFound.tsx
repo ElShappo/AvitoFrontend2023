ae26eecd930fad7c2d829d4f337a38c28351b3bd
Deni Shaipov <deni.shaipp@gmail.com>
2023-08-29 00:01:44 +0300

    Stylized NotFoundPage

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Empty, Row } from 'antd'
+import "./NotFound.css";
+import Title from 'antd/es/typography/Title';
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <Row className="NotFound" justify='center' align='middle'>
+      <Empty description={<Title level={3}>Page not found</Title>}/>
+    </Row>
   )
 }
 
