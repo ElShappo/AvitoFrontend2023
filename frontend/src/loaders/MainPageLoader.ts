118bc086d679d0f633d57e1dfde6a3ed1d7edff7
Deni Shaipov <deni.shaipp@gmail.com>
2023-08-29 21:06:48 +0300

    improving logging

--- frontend/src/loaders/MainPageLoader.ts
+++ frontend/src/loaders/MainPageLoader.ts
@@ -16,35 +16,37 @@ function mainPageLoader({request} : any) {
     if (isPlatform(platform)) {
       fetchUrl.searchParams.set('platform', platform);
     } else {
+      console.warn('Invalid or empty platform specified: substituting it with \'any platform\'');
       platform = 'any platform';
     }
     if (isGenre(genre)) {
       fetchUrl.searchParams.set('genre', genre);
     } else {
+      console.warn('Invalid or empty genre specified: substituting it with \'any genre\'');
       genre = 'any genre';
     }
     if (isSort(sort)) {
       fetchUrl.searchParams.set('sort', sort);
     } else {
+      console.warn('Invalid or empty sort specified: substituting it with \'relevance\'');
       sort = 'relevance';
     }
     if (!page || !Number.isInteger(Number(page)) || Number(page) <= 0) {
-      console.error('Smth wrong with page');
+      console.warn('Invalid or empty page No specified: substituting it with \'1');
       page = '1';
     }
     if (!isPageSize(pageSize)) {
+      console.warn('Invalid or empty pageSize specified: substituting it with \'20');
       pageSize = '20';
     }
     console.log(platform);
     console.log(genre);
     console.log(sort);
+    console.log(page);
+    console.log(pageSize);
   
     const response = fetch(fetchUrl);
-    console.log(response);
-    console.log('hey!');
 
-    console.error(page, pageSize);
-  
     return defer({
       games: response.then(res => res.json()),
       platform,
